808e129bc7fa88aac6c08e3b165a50e5228e9e9e
vinicius.sousa <vinicius.sousa@zup.com.br>
2018-11-01 10:01:51 -0300

    profiles.active

--- skeleton-web-services/src/main/java/br/zup/batch/GreetingBatchBean.java
+++ skeleton-web-services/src/main/java/br/zup/batch/GreetingBatchBean.java
@@ -5,11 +5,13 @@ import br.zup.service.GreetingService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
 
+@Profile("batch")
 @Component
 public class GreetingBatchBean {
 
@@ -18,34 +20,34 @@ public class GreetingBatchBean {
     @Autowired
     private GreetingService greetingService;
 
-//    @Scheduled(
-//            cron = "0,30 * * * * *"
-//    )
-//    public void cronJob(){
-//        logger.info("> cronJob");
-//        Collection<Greeting> greetings = greetingService.findAll();
-//        logger.info("There are {} greeting in the data store.",greetings.size());
-//
-//    }
-//
-//
-//    @Scheduled(
-//            initialDelay = 5000,
-//            fixedDelay = 15000
-//    )
-//    public void fixedDelayJobWithInitialDelay(){
-//        logger.info("> fixedDelayJobWithInitialDelay");
-//
-//        long pause = 5000;
-//        long start = System.currentTimeMillis();
-//        do{
-//            if(start + pause < System.currentTimeMillis())
-//                break;
-//        }while (true);
-//        logger.info("Processing time was {} seconds.", pause / 1000);
-//        logger.info("< fixedDelayJobWithInitialDelay");
-//
-//
-//    }
+    @Scheduled(
+            cron = "${batch.greeting.cron}"
+    )
+    public void cronJob(){
+        logger.info("> cronJob");
+        Collection<Greeting> greetings = greetingService.findAll();
+        logger.info("There are {} greeting in the data store.",greetings.size());
+
+    }
+
+
+    @Scheduled(
+            initialDelayString = "${batch.greeting.initialDelay}",
+            fixedDelayString = "${batch.greeting.fixedDelay}"
+    )
+    public void fixedDelayJobWithInitialDelay(){
+        logger.info("> fixedDelayJobWithInitialDelay");
+
+        long pause = 5000;
+        long start = System.currentTimeMillis();
+        do{
+            if(start + pause < System.currentTimeMillis())
+                break;
+        }while (true);
+        logger.info("Processing time was {} seconds.", pause / 1000);
+        logger.info("< fixedDelayJobWithInitialDelay");
+
+
+    }
 
 }
